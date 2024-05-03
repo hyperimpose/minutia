c59c87fb7bd6538992cd13345452ef594e610558
hyperimpose <hyperimpose@protonmail.com>
2024-05-04 02:51:34 +0300

    libminutia: Moved the erlang communication parts to another module erlang.py, added a custom log handler, started using the logging library.

--- python_src/libminutia/__main__.py
+++ python_src/libminutia/__main__.py
@@ -19,95 +19,25 @@
 import asyncio
 import contextlib
 import io
-import os
+import logging
 import traceback
 
-import libminutia
+from erlang import log, read_packet, send, ErlangLogHandler
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(message)s",
+    handlers=[ErlangLogHandler()]
+)
+
+
+import libminutia  # noqa
 
 
 # ====================================================================
-# Erlang
+# Minutia
 # ====================================================================
-
-# Erlang Port Settings
-FD_IN = 0
-FD_OUT = 1
-
-
-# Readers ============================================================
-
-async def read_packet(loop) -> bytes:
-    return await loop.run_in_executor(None, _read_packet)
-
-
-def _read_packet() -> bytes | None:
-    length_b = os.read(0, 2)
-    if len(length_b) != 2:  # We configure the erlang port with {packet, 2}
-        return None  # The port closed
-
-    length = int.from_bytes(length_b, "big")
-
-    packet = os.read(FD_IN, length)
-    assert len(packet) == length
-
-    return packet
-
-
-# Writers ============================================================
-
-def write_data(packet: bytes):
-    packet_len = len(packet).to_bytes(2, "big")
-    os.write(FD_OUT, packet_len)
-    os.write(FD_OUT, packet)
-
-
-def send(term):
-    write_data(bencode(term))
-
-
-def log(level, term):
-    send(("log", level, term))
-
-
-# Helpers ============================================================
-
-def bencode(term):
-    """
-    Simple recursive bencode encoder.
-
-    - Because the data in libminutia are not deeply nested, it is unlikely that
-      we will hit the recursion limit.
-    - Booleans are converted to the strings 'true' and 'false'.
-    - None is converted to the empty string.
-    """
-    if term is False:
-        return b"5:false"
-    if term is True:
-        return b"4:true"
-    if term is None:
-        return b"0:"
-    if isinstance(term, int):
-        return b"i" + str(term).encode() + b"e"
-    if isinstance(term, float):
-        return bencode(str(term).encode())
-    if isinstance(term, str):
-        return bencode(term.encode())
-    if isinstance(term, bytes):
-        return str(len(term)).encode() + b":" + term
-    if isinstance(term, list) or isinstance(term, tuple):
-        return b"l" + b"".join([bencode(x) for x in term]) + b"e"
-    if isinstance(term, dict):  # in Python 3.6+ dicts are ordered
-        acc = b"d"
-        for k, v in term.items():
-            if not isinstance(k, str) and not isinstance(k, bytes):
-                raise ValueError("Dict keys must be of type str or bytes")
-            acc += bencode(k)
-            acc += bencode(v)
-        acc += b"e"
-        return acc
-
-    raise TypeError(f"Unexpect term {term} of type {type(term)}")
-
 
 # Commands ===========================================================
 
