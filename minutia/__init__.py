bf7beafe77d26b79184e917ae7281889ac6b47bc
hyperimpose <hyperimpose@protonmail.com>
2024-10-02 16:55:01 +0300

    Version bump

--- minutia/__init__.py
+++ minutia/__init__.py
@@ -1,6 +1,6 @@
 """minutia summarizes the content of various internet services"""
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 
 # --------------------------------------------------------------------
